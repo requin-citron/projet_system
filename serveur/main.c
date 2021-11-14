d2f6043af436047e465e38b6147630f5574ef611
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-11-14 04:01:47 +0100

    commencement distribution de carte !

--- serveur/main.c
+++ serveur/main.c
@@ -7,7 +7,10 @@
 #include "serveur.h"
 #define SIZE_IN 50
 
+#define NB_CARDS 10
+
 int part_acceptUsers(char[NB_SUPPORT_USERS][SIZE_IN]);
+void part_distribCards(int,int[NB_SUPPORT_USERS][NB_CARDS]);
 char* ask(const char*,char*);
 
 int main(int argc, char** argv) {
@@ -15,7 +18,9 @@ int main(int argc, char** argv) {
     char namePlayers[NB_SUPPORT_USERS][SIZE_IN];
     int nbPlayers = part_acceptUsers(namePlayers);
     ser_sendAll("NEXT");
-    ser_sendAll("crotte\n");
+    ser_sendAll(" Welcome in the mind game !\n Your card are : ");
+    int minCardsByPlayers[nbPlayers][NB_CARDS];
+    part_distribCards(nbPlayers,minCardsByPlayers);
     ser_close();
     printf("\n");
     return EXIT_SUCCESS;
@@ -42,7 +47,11 @@ int part_acceptUsers(char namePlayers[NB_SUPPORT_USERS][SIZE_IN]) {
     }
     return nbPlayers;
 }
-
+void part_distribCards(int nbPlayers, int minCardsByPlayers[NB_SUPPORT_USERS][NB_CARDS]) {
+    for (int t=0; t<nbPlayers; t++)
+        for (int h=0; h<NB_CARDS; h++)
+            minCardsByPlayers[t][h] = 0;
+}
 char* ask(const char *intro, char *str) {
     printf("%s&> ",intro);
     fgets(str,SIZE_IN,stdin);
